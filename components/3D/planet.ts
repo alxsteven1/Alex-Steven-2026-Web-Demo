269ad66724dd2a7bdc8eca7c904adc82b7752e93
Alex Steven <alexmightyman@gmail.com>
2026-03-23 17:31:05 +0200

    Update planet.ts

--- components/3D/planet.ts
+++ components/3D/planet.ts
@@ -1,7 +1,7 @@
 
 import * as THREE from "three";
 import gsap from "gsap";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { CITIES } from "@/config/cities";
 
 import earthVertex from "./shaders/earth/vertex";
