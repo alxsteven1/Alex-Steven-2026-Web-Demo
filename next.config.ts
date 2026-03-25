410bc78225d73b46a1871ccd3a5c76e4e6459543
Alex Steven <alexmightyman@gmail.com>
2026-03-25 10:39:28 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Alex-Steven-2026-Web-Demo',
-  assetPrefix: '/Alex-Steven-2026-Web-Demo',
+  // Removed basePath and assetPrefix because we are now on a custom root domain
   images: {
     unoptimized: true,
   },
